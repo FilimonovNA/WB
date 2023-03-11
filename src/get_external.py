4f8516d7d9a47fe08de4f62816fb62dfe67fcf4e
FilimonovNA <filimonovn.a@ya.ru>
2023-03-11 21:29:00 +0300

    add 1 item_id, 1 request mode

--- src/get_external.py
+++ src/get_external.py
@@ -4,10 +4,29 @@ from constants import *
 
 class InfoImport:
 
-    def __init__(self, item_id=DEFAULT_ITEM_ID, user_request=None, user_requests_list=None):
-        self.item_id = item_id
-        self.user_request = user_request
-        self.user_requests_list = user_requests_list
+    def __init__(self): #, item_id=DEFAULT_ITEM_ID, user_request=None, user_requests_list=None):
+        self.mode = 1
+        self.item_id = None
+        self.user_request = None
+        self.user_requests_list = None
+
+    def select_mode(self):
+        mode = -1
+        while 0 > mode or mode > 4:
+            print('Выберите режим работы:\n'
+                  '0 - Выход из программы\n'
+                  '1 - Одиночный запрос для одного артикула\n'
+                  '2 - Одиночный запрос для нескольких артикулов\n'
+                  '3 - Множественный запрос для одного артикула\n'
+                  '4 - Множественный запрос для нескольких артикулов\n')
+            mode = input('Введите цифру от 0 до 4:  ')
+            try:
+                mode = int(mode)
+            except ValueError:
+                mode = -1
+                print('Это не цифра\n')
+        self.mode = mode
+        return self
 
     def get_user_request(self):
         request = input("Enter user request: ")
@@ -22,7 +41,9 @@ class InfoImport:
             return 1
 
     def get_user_requests_list(self):
-        path = f'{self.item_id}.txt'
+        #os.getcwd()
+        os.chdir('..')      # поднимаемся на одну ступень выше
+        path = f'user_requests/{self.item_id}.txt'
         if not os.path.isfile(path):
             return 1
         self.user_requests_list = []
@@ -33,3 +54,15 @@ class InfoImport:
             return self.user_requests_list
         return 1
 
+    def get_common_requests_list(self):
+        os.chdir('..')      # поднимаемся на одну ступень выше
+        path = f'user_requests/common.txt'
+        if not os.path.isfile(path):
+            return 1
+        self.user_requests_list = []
+        with open(path, "r") as file:
+            for line in file.readlines():
+                self.user_requests_list.append(line.replace(" ", "+").replace("\n", ""))
+        if len(self.user_requests_list) > 0:
+            return self.user_requests_list
+        return 1
--- src/main.py
+++ src/main.py
@@ -1,14 +1,91 @@
 from get_external import InfoImport
-from wb_parser import Request
+from wb_parser import Request, WBParser
 from constants import *
+import os
 
-info = InfoImport()
+
+#user = InfoImport()
 # info.get_user_request()
 # print(info.user_request)
 # print(info.get_user_requests_list())
+# for request_text in user_requests:
+#     for current_page in range(1, 3, 3):
+#         r = Request(DEFAULT_ITEM_ID, request_text, current_page).get_request()
+#         #d = r.get_data()
+#         print(r.request)
 
-request_text = info.get_user_request()
-for current_page in range(1, 3, 3):
-    r = Request(DEFAULT_ITEM_ID, request_text, current_page).get_request()
-    #d = r.get_data()
-    print(r.request)
+
+def get_all_requests_positions(item_id, user_requests):
+    if isinstance(user_requests, list):
+        positions_list = []
+        for request_text in user_requests:
+            positions_list.append(get_one_request_position(item_id, request_text))
+        return positions_list
+    else:
+        return get_one_request_position(item_id, user_requests)
+
+
+def get_one_request_position(item_id, request_text):
+    for current_page in range(1, MAX_PAGE):
+        request = Request(item_id, request_text, current_page).get_request()
+        response = request.get_data()
+        if response is not None:
+            position = WBParser(request.item_id, response).get_position()
+            if position != -1:
+                return position + (current_page-1) * MAX_LIMIT
+
+    return None
+
+
+def clear_terminal():
+   # os.system('cls' if os.name == 'nt' else 'clear')
+   # print(chr(27) + "[2J")
+   # os.system('clear')
+    print("\033c\033[3J")
+
+
+def mode_1(usr):
+    clear_terminal()
+    print('It\'s mode_1')
+    item_id = usr.get_item_id()
+    if item_id == 0:
+        return 0
+    request = usr.get_user_request()
+    if request == 0:
+        return 0
+    position = get_all_requests_positions(item_id, request)
+    try:
+        page = int(position)
+    except TypeError:
+        page = None
+    print(f'item_id = {item_id}, request = {request}, position = {position}, page = {page}\n')
+
+
+def mode_2():
+    print('It\'s mode_2')
+
+
+def mode_3():
+    print('It\'s mode_3')
+
+
+def mode_4():
+    print('It\'s mode_4')
+
+
+def main():
+    usr = InfoImport()
+    usr.select_mode()
+    while usr.mode != 0:
+        if usr.mode == 1:
+            mode_1(usr)
+        elif usr.mode == 2:
+            mode_2()
+        elif usr.mode == 3:
+            mode_3()
+        elif usr.mode == 4:
+            mode_4()
+        usr.select_mode()
+
+
+main()
